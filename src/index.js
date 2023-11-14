b20afb3792bb54d305d1a99ef61fef822dc31e0d
MrChae1 <ljnellam27@gmail.com>
2023-11-14 22:53:03 +0700

    Add main Landing page container

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './sass/mainStyle.scss';
+
+
+function mainComponent(){
+    const mainLanding = document.createElement('main');
+    mainLanding.classList.add('mainLanding');
+    
+    
+    
+    
+    
+    return mainLanding
+}
+
+document.body.append(mainComponent());
