b6d144efa7b114d885c8c253dac7fe31cab6f3a3
MrChae1 <ljnellam27@gmail.com>
2023-11-23 00:03:05 +0700

    Add function displayAll

--- src/displayTab.js
+++ src/displayTab.js
@@ -1,5 +1,17 @@
 import './sass/mainStyle.scss';
+import { getResponse, TrueOrFalse } from './api';
 
 
-
-
+const getData = async (location) => {
+    const response = await getResponse(location);
+    return response;
+}
+export function DisplayAll(location, sectionHeader){
+    const FromSHeader = Array.from(sectionHeader.querySelectorAll('*'));
+    getData(location).then((res) =>{
+        console.log(res);
+        FromSHeader[0].innerHTML = res.location.name;
+        FromSHeader[2].src = res.current.condition.icon;
+        FromSHeader[3].innerHTML = res.current.condition.text;
+    });
+}
