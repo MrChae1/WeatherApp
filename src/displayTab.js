079e0bc4107dd0fd6f416f56c730f37ccf781e85
MrChae1 <ljnellam27@gmail.com>
2023-11-25 00:22:51 +0700

    Display data from Api

--- src/displayTab.js
+++ src/displayTab.js
@@ -6,18 +6,26 @@ const getData = async (location) => {
     const response = await getResponse(location);
     return response;
 }
-export function DisplayAll(location, sectionDesc){
+export function DisplayAll(location, sectionDesc, articleHeader){
     getData(location).then((res) =>{
-        console.log(res);
         for(let i = 0; i < sectionDesc.length; i++){
             let newDesc = Array.from(sectionDesc[i].querySelectorAll('*'));
-
-            newDesc[2].innerHTML = res.location.name; //Location Name
-            newDesc[5].src = res.forecast.forecastday[i].day.condition.icon;
-            newDesc[6].innerHTML = res.forecast.forecastday[i].day.condition.text;
+            articleHeader.innerHTML = res.location.name; //Location Name
+            newDesc[1].innerHTML = res.forecast.forecastday[i].date;
+            newDesc[3].innerHTML = `${res.forecast.forecastday[i].day.avgtemp_c} °C`; //celsius/farah
+            newDesc[4].src = res.forecast.forecastday[i].day.condition.icon; //Weather Images
+            newDesc[5].innerHTML = res.forecast.forecastday[i].day.condition.text; //Weather description
+            newDesc[8].innerHTML = res.forecast.forecastday[i].day.avghumidity; //Humidity
+            newDesc[10].innerHTML = res.forecast.forecastday[i].day.maxwind_kph; //Wind
+            newDesc[12].innerHTML = res.forecast.forecastday[i].day.daily_chance_of_rain;//feelsLike
+            newDesc[14].innerHTML = res.forecast.forecastday[i].day.avgvis_km;//visibility
         }
-        
     });
 }
-            // newDesc[5].res.forecast.forecastday[i].day.condition.icon;
-            // newDesc[6].res.forecast.forecastday[i].day.condition.text;
+export function forDisplay(location, sectionTag){
+    console.log(location);
+    const newheader = sectionTag.querySelector('h3');
+    const newWeatherEl = Array.from(sectionTag.querySelectorAll('.weatherDescription'));
+
+    DisplayAll(location, newWeatherEl, newheader);
+}
