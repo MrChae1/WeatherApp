d32905423025ba097a3c729fe978a47588eec075
MrChae1 <ljnellam27@gmail.com>
2023-11-27 22:58:37 +0700

    Add fetch function that display when the fetch not working

---
+++ src/command.js
@@ -0,0 +1,13 @@
+
+export function notfetch(){
+    
+}
+
+const appendModal = () =>{
+    const modalDiv = document.createElement('div');
+    modalDiv.innerHTML = `
+        <section class="inside-modal">
+            <p>Please Wait it's loading...........</p>
+        </section>
+    `;
+}
