86e66ec5981d8944ef5672d45f9590364b2ef237
MrChae1 <ljnellam27@gmail.com>
2023-11-28 23:53:55 +0700

    Add fetch function that display when the api not work

--- src/command.js
+++ src/command.js
@@ -1,6 +1,7 @@
 
 export function notfetch(){
-    
+    const modalParah = modalTag.querySelector('p');
+    modalParah.innerHTML = `Sorry!!! Something Not Rigth Check Your Input or Reload The Page Thank You.`;  
 }
 
 const appendModal = () =>{
