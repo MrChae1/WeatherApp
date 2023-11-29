42f1bda0a96d1b312f2f456246c1cbe2cc81c5dc
MrChae1 <ljnellam27@gmail.com>
2023-11-29 21:43:06 +0700

    Remove function

--- src/command.js
+++ src/command.js
@@ -4,11 +4,3 @@ export function notfetch(){
     modalParah.innerHTML = `Sorry!!! Something Not Rigth Check Your Input or Reload The Page Thank You.`;  
 }
 
-const appendModal = () =>{
-    const modalDiv = document.createElement('div');
-    modalDiv.innerHTML = `
-        <section class="inside-modal">
-            <p>Please Wait it's loading...........</p>
-        </section>
-    `;
-}
